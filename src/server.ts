f3997faf64b5e72fe1e15db6726ee213563c5f06
otash16 <otajonqutbiddinov1@gmail.com>
2024-06-06 17:35:53 +0500

    integrations//express

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
-console.log("EXECUTED");
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment";
+import mongoose from "mongoose";
+import app from "./app";
 
-const currentTime = moment().format("YYYY-MM-DD");
-console.log(currentTime);
-const person: string = "Theo";
-const count: number = 100;
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`The server is running successfully on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR on connection MongoDB", err);
+  });
