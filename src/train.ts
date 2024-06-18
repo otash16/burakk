87a520d2fc43945d70dfcc71bf07956f10d9b2eb
otash16 <otajonqutbiddinov1@gmail.com>
2024-06-18 16:59:40 +0500

    J task completed

--- src/train.ts
+++ src/train.ts
@@ -1,23 +1,16 @@
-//task i
-function majorityElement(arr: number[]): number {
-  let objectFrequency: { [key: number]: number } = {};
-  for (let i = 0; i < arr.length; i++) {
-    let num = arr[i];
-    if (objectFrequency[num]) {
-      objectFrequency[num]++;
-    } else {
-      objectFrequency[num] = 1;
+// J TASk
+
+function findLongestWord(input: string): string {
+  let words: string[] = input.split(" ");
+
+  let longestWord: string = "";
+  for (const word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
     }
   }
-  let maxCount = 0;
-  let majorityElement: number | null = null;
-  for (let num in objectFrequency) {
-    if (objectFrequency[num] > maxCount) {
-      maxCount = objectFrequency[num];
-      majorityElement = parseInt(num);
-    }
-  }
-  return majorityElement!;
+
+  return longestWord;
 }
 
-console.log(majorityElement([5,3]));
+console.log(findLongestWord("BATMAN Spiderman  wewewewew"));
