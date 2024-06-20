fca30d9fec1e4868b403f8ebe317b30ad60bc6db
otash16 <otajonqutbiddinov1@gmail.com>
2024-06-20 17:15:41 +0500

    all new tasks completed succesfully

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,16 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongDB(session);
+
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 // 1-ENTRANCE
 const app = express();
 // console.log(__dirname);
@@ -14,6 +24,17 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 // 2- SESSIONS
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 6, // 3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // 3- VIEWS
 app.set("views", path.join(__dirname, "views"));
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -33,36 +34,87 @@ restaurantController.getLogin = (req: Request, res: Response) => {
     res.render("login");
   } catch (err) {
     console.log("Erro, getLogin", err);
+    res.redirect("/admin");
   }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("processSignup");
     // console.log("body:", req.body);
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
-    // TODO: SESSIONS AUTHENTICATION
+    // SESSIONS AUTHENTICATION
 
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+      res.redirect("/admin");
+    });
   } catch (err) {
     console.log("Erro, processSignup", err);
+    const message =
+      err instanceof Error ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script>alert("${message}"); window.location.replace('admin/login1')</script> `
+    );
     res.send(err);
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("processLogin");
     // console.log("body:", req.body);
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-    // TODO: SESSIONS AUTHENTICATION
+    // SESSIONS AUTHENTICATION
 
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Erro, processLogin", err);
+    const message =
+      err instanceof Error ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script>alert("${message}"); window.location.replace('admin/login1')</script> `
+    );
+    res.send(err);
+  }
+};
+
+restaurantController.logout = async (req: AdminRequest, res: Response) => {
+  try {
+    console.log("logout");
+    req.session.destroy(function () {
+      res.redirect("/admin");
+    });
+  } catch (err) {
+    console.log("Erro, processLogin", err);
+    res.redirect("/admin");
+  }
+};
+
+restaurantController.checkAuthSession = async (
+  req: AdminRequest,
+  res: Response
+) => {
+  try {
+    console.log("checkAuthSession");
+    if (req.session?.member)
+      res.send(`<script>alert("${req.session.member.memberNick}")</script> `);
+    else res.send(`<script>alert("${Message.NOT_AUTHENTICATED}")</script>`);
+  } catch (err) {
+    console.log("Erro, checkAuthSession", err);
     res.send(err);
   }
 };
