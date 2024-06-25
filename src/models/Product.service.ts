f88c1092d6ea24d0faad75f21771a0dcc02b7f35
otash16 <otajonqutbiddinov1@gmail.com>
2024-06-25 13:43:27 +0500

    new theme

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,10 +1,52 @@
-import ProductModel from "../schema/Product.service";
+import { shapeIntoMongooseObject } from "../libs/config";
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import {
+  Product,
+  ProductInput,
+  ProductUpdateInput,
+} from "../libs/types/product";
+import ProductModel from "../schema/Product.model";
 
 class ProductService {
   private readonly productModel;
 
   constructor() {
     this.productModel = ProductModel;
+  }
+
+  // SPA
+
+  // BSSR
+
+  public async getAllProducts(): Promise<Product[]> {
+    const result = await this.productModel.find().exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+    try {
+      return await this.productModel.create(input);
+    } catch (err) {
+      console.error("Error, model:createNewProduct:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED);
+    }
+  }
+
+  public async updateChosenProduct(
+    id: string,
+    input: ProductUpdateInput
+  ): Promise<Product> {
+    id = shapeIntoMongooseObject(id);
+    const result = await this.productModel
+      .findOneAndUpdate({ _id: id }, input, { new: true })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+    return result;
   }
 }
 
