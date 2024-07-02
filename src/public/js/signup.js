3597a0d9a16e68b36aaef5ff795a0ec997339bf6
otash16 <otajonqutbiddinov1@gmail.com>
2024-07-02 15:45:28 +0500

    EJS - Frontend Products va Users

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -5,15 +5,13 @@ $(function () {
 
   fileTarget.on("change", function () {
     if (window.FileReader) {
-      const uploadFile = $(this)[0].files[0];
-      //   console.log("uploadFile:", uploadFile);
-      const fileType = uploadFile["type"];
-      const validImageType = ["image/jpg,", "image/jpeg,", "image/png,"];
-      if (validImageType.includes(fileType)) {
+      const uploadFile = $(this)[0].files[0],
+        fileType = uploadFile["type"],
+        validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
         alert("Please insert only jpg, jpeg, and png!");
       } else {
         if (uploadFile) {
-          console.log(URL.createObjectURL(uploadFile));
           $(".upload-img-frame")
             .attr("src", URL.createObjectURL(uploadFile))
             .addClass("succes");
@@ -27,10 +25,10 @@ $(function () {
 });
 
 function validateSignupForm() {
-  const memberNick = $(".member-nick").val();
-  const memberPhone = $(".member-phone").val();
-  const memberPassword = $(".member-password").val();
-  const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
   if (
     memberNick === "" ||
--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,25 @@
 console.log("Users frontend javascript file");
+$(function () {
+  $(".member-status").on("change", function (e) {
+    const id = e.target.id,
+      memberStatus = $(`#${id}.member-status`).val();
+
+    // TODO: Axios updateChoseUser
+
+    axios
+      .post("/admin/user/edit", { _id: id, memberStatus: memberStatus })
+      .then((response) => {
+        console.log("response:", response);
+        const result = response.data;
+
+        if (result.data) {
+          console.log("User updated!");
+          $(".member-status").blur();
+        } else alert("User update failed");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("User update failed");
+      });
+  });
+});
