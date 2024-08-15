e452d2852ce90b68e67e83292cef3d8c3b0beecd
otash16 <otajonqutbiddinov1@gmail.com>
2024-08-15 17:47:00 +0500

    TAK ZH completed

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,24 @@
-function convertToSnakeCase(input: string): string {
-  return input.trim().toLowerCase().replace(/\s+/g, "_");
+// function convertToSnakeCase(input: string): string {
+//   return input.trim().toLowerCase().replace(/\s+/g, "_");
+// }
+
+// console.log(convertToSnakeCase("name should be a string"));
+
+
+
+//TASK ZH#
+
+function findDisappearedNumbers(input: number[]): number[] {
+  const largestNum = Math.max(...input);
+
+  let newArr: number[] = [];
+  for (let i = 1; i <= largestNum; i++) {
+    newArr.push(i);
+  }
+
+  let disappearedNumArr = newArr.filter((number) => !input.includes(number));
+
+  return disappearedNumArr;
 }
 
-console.log(convertToSnakeCase("name should be a string"));
+console.log(findDisappearedNumbers([1, 3, 4, 7]));
